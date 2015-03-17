fe5aaf9dd9544c213c37e187665afaa7f7f43beb
feyyazesat <feyyazesat@gmail.com>
2015-03-17 11:01:01 +0200

    test for simmlib initialized.

---
+++ simmlib/simmlib_test.go
@@ -0,0 +1,43 @@
+package simmlib
+
+import "testing"
+
+func TestInitialize(t *testing.T) {
+
+}
+
+func TestUninitialize(t *testing.T) {
+}
+
+func TestPush(t *testing.T) {
+}
+
+func TestQuery(t *testing.T) {
+}
+
+func TestGetCurrentTimeStamp(t *testing.T) {
+}
+
+func TestGetSecFromRelativeTime(t *testing.T) {
+}
+
+func TestGetResolutionKey(t *testing.T) {
+}
+
+func TestGetResolution(t *testing.T) {
+}
+
+func TestgetTimeStampsForPush(t *testing.T) {
+}
+
+func TestgetTimeStampsForQuery(t *testing.T) {
+}
+
+func TestgetEventKey(t *testing.T) {
+}
+
+func TestroundTime(t *testing.T) {
+}
+
+func Test_range(t *testing.T) {
+}
