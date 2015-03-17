a165b3eea4cfa18965ab809138ce041fa1ee00b8
feyyazesat <feyyazesat@gmail.com>
2015-03-17 11:00:36 +0200

    simmlib.go improved. Implementation done, refactoring and bug fixing needed.

--- simmlib/simmlib.go
+++ simmlib/simmlib.go
@@ -19,6 +19,11 @@ type (
 	handlerVars struct {
 		redisConnection *redisLib.Conn
 	}
+
+	TstampValTuple struct {
+		x uint64
+		y uint8
+	}
 )
 
 // !private
@@ -65,7 +70,19 @@ func Check(err error) {
 	}
 }
 
-func Push(event string, increment uint64, now uint64) (interface{}, error) {
+func Initialize() {
+	redisConnection, err := redisLib.Dial("tcp", fmt.Sprintf("%s:%s", RedisArgs.RedisHost, RedisArgs.RedisPort))
+	hnVars.redisConnection = &redisConnection
+	Check(err)
+}
+
+func Uninitialize() {
+	redisConnection := *hnVars.redisConnection
+	redisConnection.Close()
+}
+
+// Push sends HINCRBY command to redis according to given event, increment and now(timestamp) value, It works with pipline.
+func Push(event string, increment, now uint64) (interface{}, error) {
 
 	redisConnection := *hnVars.redisConnection
 	var key string
@@ -78,32 +95,22 @@ func Push(event string, increment uint64, now uint64) (interface{}, error) {
 	return redisConnection.Do("EXEC")
 }
 
-// RoundTime rounds given time according to given resolution which defined in resolutions hash map
-func RoundTime(time uint64, resolution uint64) uint64 {
-	return uint64(time - (time % resolution))
-}
+func Query(event string, start, end uint64, resolutionKey string) []TstampValTuple {
+	redisConnection := *hnVars.redisConnection
+	eventKey := getEventKey(event, resolutionKey)
+	timestamps := getTimeStampsForQuery(start, end, resolutions[resolutionKey])
+	values, err := redisConnection.Do("HMGET", eventKey, timestamps)
+	fmt.Println(values)
+	Check(err)
+	tuple := make([]TstampValTuple, len(timestamps))
 
-// getEventKey returns the event key according to given event name and resolution key
-// !private
-func getEventKey(event string, resolutionKey string) string {
-	return fmt.Sprintf("simmetrica:%s:%s", event, resolutionKey)
-}
-
-// getTimeStampsForPush yields timestamps for each resolution which defined in resolutions hash map
-// !private
-func getTimeStampsForPush(now uint64) chan TimeResolution {
-	if now == 0 {
-		now = GetCurrentTimeStamp()
+	for key, timestamp := range timestamps {
+		tuple = append(tuple, TstampValTuple{x: timestamp, y: 0})
+		//values.(string)[key]
+		_, _ = values, key
 	}
-	timeResolution := make(chan TimeResolution)
-	go func() {
-		for resolution, timestamp := range resolutions {
-			timeResolution <- TimeResolution{resolution: resolution, timestamp: RoundTime(now, timestamp)}
-		}
-		close(timeResolution)
-	}()
 
-	return timeResolution
+	return tuple
 }
 
 // GetCurrentTimeStamp returns current timestamp as type uint64
@@ -129,13 +136,68 @@ func GetSecFromRelativeTime(timespan string) uint64 {
 	return resolutions["day"] * multiplier
 }
 
-func Initialize() {
-	redisConnection, err := redisLib.Dial("tcp", fmt.Sprintf("%s:%s", RedisArgs.RedisHost, RedisArgs.RedisPort))
-	hnVars.redisConnection = &redisConnection
-	Check(err)
+func GetResolutionKey(resolutionKey string) string {
+	if resolutionKey != "" {
+		return resolutionKey
+	}
+	return DEFAULT_RESOLUTION
 }
 
-func Uninitialize() {
-	redisConnection := *hnVars.redisConnection
-	redisConnection.Close()
+func GetResolution(resolutionKey string) uint64 {
+	resolution, ok := resolutions[resolutionKey]
+	if ok {
+		return resolution
+	}
+	return resolutions[DEFAULT_RESOLUTION]
+}
+
+// getTimeStampsForPush yields timestamps for each resolution which defined in resolutions hash map
+// !private
+func getTimeStampsForPush(now uint64) chan TimeResolution {
+	if now == 0 {
+		now = GetCurrentTimeStamp()
+	}
+	timeResolution := make(chan TimeResolution)
+	go func() {
+		defer close(timeResolution)
+		for resolution, timestamp := range resolutions {
+			timeResolution <- TimeResolution{resolution: resolution, timestamp: roundTime(now, timestamp)}
+		}
+	}()
+
+	return timeResolution
+}
+
+func getTimeStampsForQuery(start, end, resolution uint64) []uint64 {
+	return _range(roundTime(start, resolution), roundTime(end, resolution), resolution)
+}
+
+// getEventKey returns the event key according to given event name and resolution key
+// !private
+func getEventKey(event, resolutionKey string) string {
+	return fmt.Sprintf("simmetrica:%s:%s", event, resolutionKey)
+}
+
+// roundTime rounds given time according to given resolution which defined in resolutions hash map
+func roundTime(time, resolution uint64) uint64 {
+	return uint64(time - (time % resolution))
+}
+
+// _range python like range, _func it is not idiomatic in go, need to seperate than built-in range.
+func _range(start, end, frequency uint64) []uint64 {
+	diff := start - end
+	if diff <= 0 {
+		return make([]uint64, 1)
+	}
+	//	fmt.Println("Start : ", start, "End : ", end)
+	rangeSlice := make([]uint64, (0-diff)/frequency)
+	current := start
+	for current < end {
+		current = current + frequency
+		if current < end {
+			rangeSlice = append(rangeSlice, current)
+		}
+		//		fmt.Println("Inc : ", start)
+	}
+	return rangeSlice
 }
